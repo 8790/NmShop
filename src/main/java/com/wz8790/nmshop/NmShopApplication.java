26759da4de3e550c13b06c234c695f68f63f4a38
8790 <2467042336@qq.com>
2020-09-14 10:55:05 +0800

    Init project

---
+++ src/main/java/com/wz8790/nmshop/NmShopApplication.java
@@ -0,0 +1,13 @@
+package com.wz8790.nmshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wz8790/nmshop/NmShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wz8790.nmshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
