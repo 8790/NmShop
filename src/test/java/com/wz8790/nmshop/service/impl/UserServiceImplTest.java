b75ff248ac99e759ee158c1d9ffe2df3f14d9ff5
8790 <2467042336@qq.com>
2020-09-16 13:04:32 +0800

    补全用户模块的单元测试

--- src/test/java/com/wz8790/nmshop/service/impl/UserServiceImplTest.java
+++ src/test/java/com/wz8790/nmshop/service/impl/UserServiceImplTest.java
@@ -2,8 +2,13 @@ package com.wz8790.nmshop.service.impl;
 
 import com.wz8790.nmshop.NmShopApplicationTests;
 import com.wz8790.nmshop.enums.RoleEnum;
+import com.wz8790.nmshop.enums.StatusCodeEnum;
 import com.wz8790.nmshop.pojo.User;
 import com.wz8790.nmshop.service.IUserService;
+import com.wz8790.nmshop.vo.ResponseVo;
+import com.wz8790.nmshop.vo.UserVo;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,12 +16,22 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserServiceImplTest extends NmShopApplicationTests {
 
+    public static final String USERNAME = "小明";
+    public static final String PASSWORD = "1234";
+
     @Autowired
     private IUserService userService;
 
     @Test
+    @Before
     public void register() {
-        User user = new User("小明", "1234", "xm@qq.com", RoleEnum.CUSTOMER.getCode());
+        User user = new User(USERNAME, PASSWORD, "xm@qq.com", RoleEnum.CUSTOMER.getCode());
         userService.register(user);
+    }
+
+    @Test
+    public void login() {
+        ResponseVo<UserVo> login = userService.login(USERNAME, PASSWORD);
+        Assert.assertEquals(StatusCodeEnum.SUCCESS.getCode(), login.getStatus());
     }
 }
