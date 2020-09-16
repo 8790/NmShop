2a8569f2c358506613e24d904fe5625f441689b4
8790 <2467042336@qq.com>
2020-09-16 15:53:17 +0800

    分类模块，添加所有子节点，并对分类进行正确的排序

--- src/main/java/com/wz8790/nmshop/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/wz8790/nmshop/service/impl/CategoryServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,15 +35,41 @@ public class CategoryServiceImpl implements ICategoryService {
 //                categoryVos.add(categoryVo);
 //            }
 //        }
+//        categoryVos.sort(Comparator.comparing(CategoryVo::getSortOrder).reversed());
 
         //使用lambda + stream 的方式
         List<CategoryVo> categoryVos = categories.stream()
                 .filter(e -> e.getParentId().equals(NmShopConsts.ROOT_PARENT_ID))
                 .map(this::category2CategoryVo)
+                .sorted(Comparator.comparing(CategoryVo::getSortOrder).reversed())
                 .collect(Collectors.toList());
 
+        //查出子分类
+        findSubCategory(categoryVos, categories);
 
         return ResponseVo.success(categoryVos);
+    }
+
+    /**
+     * 查出子分类，使用递归的方式
+     * @param categoryVos 要查出的子分类,其中的数据都是父节点
+     * @param categories 所有分类数据的数据集
+     */
+    private void findSubCategory(List<CategoryVo> categoryVos, List<Category> categories) {
+        for (CategoryVo categoryVo : categoryVos) {
+            List<CategoryVo> subCategoryVos = new ArrayList<>();
+            for (Category category : categories) {
+                //查到内容了！
+                if (categoryVo.getId().equals(category.getParentId())) {
+                    subCategoryVos.add(category2CategoryVo(category));
+                }
+            }
+
+            subCategoryVos.sort(Comparator.comparing(CategoryVo::getSortOrder).reversed());
+            findSubCategory(subCategoryVos, categories);
+            categoryVo.setSubCategories(subCategoryVos);
+
+        }
     }
 
     private CategoryVo category2CategoryVo(Category category) {
