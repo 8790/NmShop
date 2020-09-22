ae56c5336b6941f80f840941108b352c7bdcebfa
8790 <2467042336@qq.com>
2020-09-22 14:49:55 +0800

    bugfix: User更新为UserVo

--- src/main/java/com/wz8790/nmshop/controller/OrderController.java
+++ src/main/java/com/wz8790/nmshop/controller/OrderController.java
@@ -3,10 +3,10 @@ package com.wz8790.nmshop.controller;
 import com.github.pagehelper.PageInfo;
 import com.wz8790.nmshop.consts.NmShopConsts;
 import com.wz8790.nmshop.form.OrderCreateForm;
-import com.wz8790.nmshop.pojo.User;
 import com.wz8790.nmshop.service.IOrderService;
 import com.wz8790.nmshop.vo.OrderVo;
 import com.wz8790.nmshop.vo.ResponseVo;
+import com.wz8790.nmshop.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,8 +23,8 @@ public class OrderController {
     public ResponseVo<OrderVo> create(@Valid @RequestBody OrderCreateForm form,
                                       HttpSession session) {
 
-        User user = (User) session.getAttribute(NmShopConsts.CURRENT_USER);
-        return orderService.create(user.getId(), form.getShippingId());
+        UserVo userVo = (UserVo) session.getAttribute(NmShopConsts.CURRENT_USER);
+        return orderService.create(userVo.getId(), form.getShippingId());
     }
 
     @GetMapping("/orders")
@@ -32,21 +32,21 @@ public class OrderController {
                                      @RequestParam(required = false, defaultValue = "10") Integer pageSize,
                                      HttpSession session) {
 
-        User user = (User) session.getAttribute(NmShopConsts.CURRENT_USER);
-        return orderService.list(user.getId(), pageNum, pageSize);
+        UserVo userVo = (UserVo) session.getAttribute(NmShopConsts.CURRENT_USER);
+        return orderService.list(userVo.getId(), pageNum, pageSize);
     }
 
     @GetMapping("/orders/{orderNo}")
     public ResponseVo detail(@PathVariable Long orderNo, HttpSession session) {
 
-        User user = (User) session.getAttribute(NmShopConsts.CURRENT_USER);
-        return orderService.detail(user.getId(), orderNo);
+        UserVo userVo = (UserVo) session.getAttribute(NmShopConsts.CURRENT_USER);
+        return orderService.detail(userVo.getId(), orderNo);
     }
 
     @PutMapping("/orders/{orderNo}")
     public ResponseVo cancel(@PathVariable Long orderNo, HttpSession session) {
 
-        User user = (User) session.getAttribute(NmShopConsts.CURRENT_USER);
-        return orderService.cancel(user.getId(), orderNo);
+        UserVo userVo = (UserVo) session.getAttribute(NmShopConsts.CURRENT_USER);
+        return orderService.cancel(userVo.getId(), orderNo);
     }
-} 
+}
