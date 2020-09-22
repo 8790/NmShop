bcc9eaa07745baad1add70fe707b0813ddb2ba6e
8790 <2467042336@qq.com>
2020-09-22 10:18:52 +0800

    订单模块：创建订单完成

--- src/main/java/com/wz8790/nmshop/service/impl/OrderServiceImpl.java
+++ src/main/java/com/wz8790/nmshop/service/impl/OrderServiceImpl.java
@@ -1,24 +1,28 @@
 package com.wz8790.nmshop.service.impl;
 
+import com.wz8790.nmshop.dao.OrderItemMapper;
+import com.wz8790.nmshop.dao.OrderMapper;
 import com.wz8790.nmshop.dao.ProductMapper;
 import com.wz8790.nmshop.dao.ShippingMapper;
+import com.wz8790.nmshop.enums.OrderStatusEnum;
+import com.wz8790.nmshop.enums.PaymentTypeEnum;
 import com.wz8790.nmshop.enums.ProductStatusEnum;
 import com.wz8790.nmshop.enums.StatusCodeEnum;
-import com.wz8790.nmshop.pojo.Cart;
-import com.wz8790.nmshop.pojo.Product;
-import com.wz8790.nmshop.pojo.Shipping;
+import com.wz8790.nmshop.pojo.*;
 import com.wz8790.nmshop.service.ICartService;
 import com.wz8790.nmshop.service.IOrderService;
+import com.wz8790.nmshop.vo.OrderItemVo;
 import com.wz8790.nmshop.vo.OrderVo;
 import com.wz8790.nmshop.vo.ResponseVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.math.BigDecimal;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,11 +34,18 @@ public class OrderServiceImpl implements IOrderService {
     @Resource
     private ProductMapper productMapper;
 
+    @Resource
+    private OrderMapper orderMapper;
+
+    @Resource
+    private OrderItemMapper orderItemMapper;
+
     @Autowired
     private ICartService cartService;
 
 
     @Override
+    @Transactional
     public ResponseVo<OrderVo> create(Integer uid, Integer shippingId) {
         // 1.查出收货地址
         Shipping shipping = shippingMapper.selectByShippingIdAndUid(uid, shippingId);
@@ -68,6 +79,9 @@ public class OrderServiceImpl implements IOrderService {
                 .collect(Collectors.toMap(Product::getId, product -> product));
 
         //  开始校验商品以及库存
+        List<OrderItem> orderItemList = new ArrayList<>();
+        Long orderNo = generateOrderNo();
+
         for (Cart cart : cartList) {
             Product product = map.get(cart.getProductId());
             // 是否有商品
@@ -82,21 +96,102 @@ public class OrderServiceImpl implements IOrderService {
             if (product.getStock() < cart.getQuantity()) {
                 return ResponseVo.error(StatusCodeEnum.PRODUCT_STOCK_ERROR, product.getName() + " 库存不足");
             }
+            // 减库存
+            product.setStock(product.getStock() - cart.getQuantity());
+            int row = productMapper.updateByPrimaryKeySelective(product);
+            if (row <= 0) {
+                return ResponseVo.error(StatusCodeEnum.SERVER_ERROR);
+            }
+
+            //构造orderItem
+            OrderItem item = buildOrderItem(uid, orderNo, cart.getQuantity(), product);
+            orderItemList.add(item);
 
         }
 
-
-        // 3.计算总价格（只计算被选中的商品）
+        // 3.构造Order计算总价格（计算的是被选中的商品）
+        Order order = buildOrder(uid, orderNo, shippingId, orderItemList);
 
         // 4.生成订单，入库，因为要同时写入两个表，所以要使用事务的方式控制
+        int orderRow = orderMapper.insertSelective(order);
+        if (orderRow  <= 0) {
+            return ResponseVo.error(StatusCodeEnum.SERVER_ERROR);
+        }
 
-        // 5.减库存
+        int orderItemRow = orderItemMapper.batchInsert(orderItemList);
+        if (orderItemRow  <= 0) {
+            return ResponseVo.error(StatusCodeEnum.SERVER_ERROR);
+        }
 
-        // 6.清除已经下单了的购物车
+        // 5.清除已经下单了的购物车
+        // Redis的事务不能回滚，所以放在最后
+        for (Cart cart : cartList) {
+            cartService.delete(uid, cart.getProductId());
+        }
 
-        // 7.构造vo返回给前端
-
-
-        return null;
+        // 6.构造vo返回给前端
+        OrderVo orderVo = buildOrderVo(order, orderItemList, shipping);
+        return ResponseVo.success(orderVo);
     }
+
+    private OrderVo buildOrderVo(Order order, List<OrderItem> orderItemList, Shipping shipping) {
+        OrderVo orderVo = new OrderVo();
+        BeanUtils.copyProperties(order, orderVo);
+
+        List<OrderItemVo> orderItemVoList = orderItemList.stream().map(e -> {
+            OrderItemVo orderItemVo = new OrderItemVo();
+            BeanUtils.copyProperties(e, orderItemVo);
+            return orderItemVo;
+        }).collect(Collectors.toList());
+        orderVo.setOrderItemVoList(orderItemVoList);
+
+        orderVo.setShippingId(shipping.getId());
+        orderVo.setShippingVo(shipping);
+
+        return orderVo;
+    }
+
+
+    private Order buildOrder(Integer uid,
+                             Long orderNo,
+                             Integer shippingId,
+                             List<OrderItem> orderItemList) {
+
+        BigDecimal payment = orderItemList.stream()
+                .map(OrderItem::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        Order order = new Order();
+        order.setOrderNo(orderNo);
+        order.setUserId(uid);
+        order.setShippingId(shippingId);
+        order.setPayment(payment);
+        order.setPaymentType(PaymentTypeEnum.PAY_ONLINE.getCode());
+        order.setPostage(0);
+        order.setStatus(OrderStatusEnum.NO_PAY.getCode());
+
+        return order;
+    }
+
+    /**
+     * 企业级要使用分布式唯一id的方式解决
+     * @return
+     */
+    private Long generateOrderNo() {
+        return System.currentTimeMillis() * 1000 + new Random().nextInt(999);
+    }
+
+    private OrderItem buildOrderItem(Integer uid, Long orderNo, Integer quantity, Product product) {
+        OrderItem item = new OrderItem();
+        item.setUserId(uid);
+        item.setOrderNo(orderNo);
+        item.setProductId(product.getId());
+        item.setProductName(product.getName());
+        item.setProductImage(product.getMainImage());
+        item.setCurrentUnitPrice(product.getPrice());
+        item.setQuantity(quantity);
+        item.setTotalPrice(product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+        return item;
+    }
+
 }
---
+++ src/test/java/com/wz8790/nmshop/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.wz8790.nmshop.service.impl;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.wz8790.nmshop.NmShopApplicationTests;
+import com.wz8790.nmshop.enums.StatusCodeEnum;
+import com.wz8790.nmshop.form.CartAddForm;
+import com.wz8790.nmshop.service.ICartService;
+import com.wz8790.nmshop.service.IOrderService;
+import com.wz8790.nmshop.vo.CartVo;
+import com.wz8790.nmshop.vo.OrderVo;
+import com.wz8790.nmshop.vo.ResponseVo;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Slf4j
+public class OrderServiceImplTest extends NmShopApplicationTests {
+
+    @Autowired
+    private IOrderService orderService;
+
+    @Autowired
+    private ICartService cartService;
+
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private Integer uid = 1;
+
+    private Integer shippingId = 5;
+
+    private Integer productId = 27;
+
+    @Before
+    public void addCart() {
+        CartAddForm cartAddForm = new CartAddForm();
+        cartAddForm.setProductId(productId);
+        cartAddForm.setSelected(true);
+        ResponseVo<CartVo> responseVo = cartService.add(uid, cartAddForm);
+        Assert.assertEquals(StatusCodeEnum.SUCCESS.getCode(), responseVo.getStatus());
+    }
+
+    @Test
+    public void create() {
+        ResponseVo<OrderVo> responseVo = orderService.create(uid, shippingId);
+        log.info("response = {}", gson.toJson(responseVo));
+        Assert.assertEquals(StatusCodeEnum.SUCCESS.getCode(), responseVo.getStatus());
+    }
+
+}
